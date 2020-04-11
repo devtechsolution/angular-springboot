aa5b19556569be9f2a37b26f94b0f3af6a795bd6
devtechsolution <srivastvadevzone@gmail.com>
2020-04-12 01:01:08 +0530

    Login With Hard coded authentication and handling error with *ngIf directive

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -9,13 +9,20 @@ export class LoginComponent implements OnInit {
 
   username = 'devtechsolution';
   password = '';
+  errorMessage = 'Invalid Credentials';
+  invalidLogin = false ;
   constructor() { }
 
   ngOnInit() {
   }
 
   handleLogin() {
-    console.log(this.username);
+    // console.log(this.username);
+    if (this.username === 'devtechsolution' && this.password === 'Rama') {
+      this.invalidLogin = false ;
+    } else {
+      this.invalidLogin = true ;
+    }
   }
 
 }
