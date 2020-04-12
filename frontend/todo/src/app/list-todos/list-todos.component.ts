dbc8633f49b77ffefca1b44100c4598eedda3be9
devtechsolution <srivastvadevzone@gmail.com>
2020-04-12 21:36:19 +0530

    Create link on welcome page to show todo-list

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Todo {
+
+  constructor(
+    public id: number,
+    public description: string,
+    public done:  boolean,
+    public targetdate: Date
+     ) {}
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +19,12 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    { id: 1,  description: 'Learn Angula'  },
-    { id: 2,  description: 'Learn Angula Fullstack'  },
-    { id: 2,  description: 'Learn Spring Boot'  }
+    new Todo(1, 'Learn Angular', false, new Date()),
+    new Todo(1, 'Learn Angula Fullstack', false, new Date()),
+    new Todo(1, 'Learn Spring Boot', false, new Date())
+    // { id: 1,  description: 'Learn Angula'  },
+    // { id: 2,  description: 'Learn Angula Fullstack'  },
+    // { id: 2,  description: 'Learn Spring Boot'  }
 
   ];
 
