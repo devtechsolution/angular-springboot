d1c6970c55ede2826fdd7708fc4e99e08d08a78f
devtechsolution <srivastvadevzone@gmail.com>
2020-04-12 16:45:44 +0530

    Implementing Routing from Login to Welcome Page

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,17 @@ export class LoginComponent implements OnInit {
   password = '';
   errorMessage = 'Invalid Credentials';
   invalidLogin = false ;
-  constructor() { }
+
+  //  Router
+  //  Angular.giveMeRouter
+  //  Dependency Injection which is in-built feature in Angular.
+  /*  Here Router is a dependency.
+      So to get this dependency we have to paas it in constructor as an argument.
+      In TypeScript whenever we pass a parameter to Constructor, 
+      that parameter would be available as a member variable 
+  */
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
@@ -19,6 +30,12 @@ export class LoginComponent implements OnInit {
   handleLogin() {
     // console.log(this.username);
     if (this.username === 'devtechsolution' && this.password === 'Rama') {
+      //  Redirect to Welcome Page using navigate() method of Router.
+      //  navigate() method help us to route to a specific page.
+      //  navigate() method accept an array and the first element inside this array 
+      //  the page we want to navigate. We can pass parameter along with router.
+      this.router.navigate(['welcome']);
+
       this.invalidLogin = false ;
     } else {
       this.invalidLogin = true ;
