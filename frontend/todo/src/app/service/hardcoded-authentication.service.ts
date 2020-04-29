fb62b1eca33d3256eaab96603575b87c51894dbc
devtechsolution <srivastvadevzone@gmail.com>
2020-04-29 13:49:05 +0530

    Implementig Session and enabled menus for loggedin user

--- frontend/todo/src/app/service/hardcoded-authentication.service.ts
+++ frontend/todo/src/app/service/hardcoded-authentication.service.ts
@@ -8,11 +8,18 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password){
-
+    console.log( 'Before: ' + this.isUserLoggedIn());
     if (username === 'devtechsolution' && password === 'Rama') {
+      sessionStorage.setItem('authenticateUser', username);
+      console.log( 'After: ' + this.isUserLoggedIn());
       return true;
     }
     return false;
 
+  }
+
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem('authenticateUser')
+    return !(user === null );
   }
 }
