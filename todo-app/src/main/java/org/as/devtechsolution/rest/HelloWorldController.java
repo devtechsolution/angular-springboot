ec3d7c190d9c2b527a24fa5afe75bbda1e540040
devtechsolution <srivastvadevzone@gmail.com>
2020-05-02 21:58:16 +0530

    Added CrossOrigins annotaion

--- todo-app/src/main/java/org/as/devtechsolution/rest/HelloWorldController.java
+++ todo-app/src/main/java/org/as/devtechsolution/rest/HelloWorldController.java
@@ -1,7 +1,5 @@
 package org.as.devtechsolution.rest;
 
-import javax.annotation.Resource;
-
 import org.as.devtechsolution.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(value= {"http://localhost:4200/"})
+@CrossOrigin(origins= {"http://localhost:4200"})
 public class HelloWorldController {
 	
 	@GetMapping(path="/hello-world")
