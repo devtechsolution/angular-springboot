f50c07d34d07d5b96e392fe89971920ab81f27b7
devtechsolution <srivastvadevzone@gmail.com>
2020-05-02 09:11:32 +0530

    Todo Backend for HelloWorld

---
+++ todo-app/src/main/java/org/as/devtechsolution/rest/HelloWorldController.java
@@ -0,0 +1,34 @@
+package org.as.devtechsolution.rest;
+
+import javax.annotation.Resource;
+
+import org.as.devtechsolution.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(value= {"http://localhost:4200/"})
+public class HelloWorldController {
+	
+	@GetMapping(path="/hello-world")
+	private String helloWorld() {
+		return "Hello World Backend";
+
+	}
+	
+	
+	@GetMapping(path="/hello-world-bean")
+	private HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	private HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+
+	}
+
+}
