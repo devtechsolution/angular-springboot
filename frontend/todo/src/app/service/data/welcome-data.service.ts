7ac32669d3a1e8397cd39e4f35b91cce91c2f58b
devtechsolution <srivastvadevzone@gmail.com>
2020-05-02 21:53:05 +0530

    Consume Rest API using HTTPClient using Observable and subscribe

---
+++ frontend/todo/src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+export class HelloWorldBean{
+  constructor(public message:string){}
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeDataService {
+
+  constructor(private http: HttpClient
+  ) { }
+
+  executeHelloWorldBeanService() {
+    // console.log('Execute Hello World Bean Service');
+    return this.http.get<HelloWorldBean>('http://localhost:8081/hello-world-bean');
+  }
+}
--- frontend/todo/src/app/welcome/welcome.component.ts
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,16 +9,44 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
 
-  message = 'some ';
+  message = 'some Welcome Message';
+  welcomeMessageFromService: string;
   name = '';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private service: WelcomeDataService
+    ) { }
 
   ngOnInit() {
 
     // console.log(this.route.snapshot.params['name']);
 
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelomemessage() {
+    // console.log('Get Welcome Message');
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulRessponse(response),
+      error => this.handleErrorRessponse(error)
+      //response=> console.log(response.message)
+    );
+    //console.log(this.service.executeHelloWorldBeanService());
+  }
+
+  handleSuccessfulRessponse(response){
+    this.welcomeMessageFromService = response.message;
+    // console.log(response);
+    // console.log(response.message);
+  }
+
+  handleErrorRessponse(error) {
+    this.welcomeMessageFromService = error.error.message;
+    console.log(error);
+    console.log(error.error);
+    console.log(error.error.message);
+    // console.log(response);
+    // console.log(response.message);
   }
 
 }
