5e27c5fd07b3e4f50bc51f487800ba20abac8004
devtechsolution <srivastvadevzone@gmail.com>
2020-05-03 23:31:46 +0530

    Implementeing basic authentication with Options error

--- frontend/todo/src/app/service/data/welcome-data.service.ts
+++ frontend/todo/src/app/service/data/welcome-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 export class HelloWorldBean {
   constructor(public message: string) {}
@@ -19,7 +19,33 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldServiceWithPathVariable(name) {
-    // console.log('Execute Hello World Bean Service');
-    return this.http.get<HelloWorldBean>(`http://localhost:8081//hello-world/path-variable/${name}`);
+    let basicAuthHeaderString = this.createBasicAuthenticaationHttpHeader();
+    // let passheaders = 
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString;
+    })
+
+    // return this.http.get<HelloWorldBean>(`http://localhost:8081//hello-world/path-variable/${name}`,
+    // {headers: passheaders});
+    return this.http.get<HelloWorldBean>(`http://localhost:8081//hello-world/path-variable/${name}`,
+     {headers});
   }
+
+  createBasicAuthenticaationHttpHeader() {
+    let username = 'devtechsoution';
+    let password = 'Rama';
+    let basicAuthHeaderString = 'Basic' + window.btoa(username + ':' + password);
+    return basicAuthHeaderString;
+  }
+
+  // Access to XMLHttpRequest at
+  //  'http://localhost:8081//hello-world/path-variable/devtechsolution' from origin 
+  // 'http://localhost:4200' has been blocked by CORS policy:
+  //  No 'Access-Control-Allow-Origin' header is present on the requested resource.
+
+
+  // Access to XMLHttpRequest at 'http://localhost:8081//hello-world/path-variable/devtechsolution' 
+  // from origin 'http://localhost:4200' has been blocked by CORS policy:
+  // Response to preflight request doesn't pass access control check:
+  // No 'Access-Control-Allow-Origin' header is present on the requested resource.
 }
